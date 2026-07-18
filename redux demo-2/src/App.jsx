e1799a4de1ef60577a83965a5eaaae25151a277e
MohamedYashar <arrafathyas@gmail.com>
2026-07-18 19:05:26 +0530

    added redux demo-2 project folder

---
+++ redux demo-2/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import ProductList from "./components/ProductList";
+
+export default function App() {
+  return (
+    <div>
+      <h1> Redux-Demo</h1>
+      <ProductList />
+    </div>
+  );
+}
